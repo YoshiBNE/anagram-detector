f866623080c3ef0a356bc9e5274363a3bccd29f1
YoshiBNE <yoshiyasu_express@me.com>
2022-07-06 00:19:03 +1000

    done

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,20 @@
 # Your code goes here!
+class Anagram
+
+    attr_accessor :word
+
+    def initialize(word)
+        @word = word
+    end
+
+    def match(array)
+        result = []
+        keyword = word.split("")
+        array.each do |inside_word|
+            if keyword.sort == inside_word.split("").sort
+                result << inside_word
+            end
+        end
+        result
+    end
+end
